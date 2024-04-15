a807ec24c6147a45fe7d7127d9e1644e4f9c9fbb
karpik11 <163449991+karpik11@users.noreply.github.com>
2024-04-15 22:31:26 +0300

    add project-wake-bake-111-120

---
+++ project-wake-bake-111-120/js/main.js
@@ -0,0 +1,177 @@
+'use strict';
+
+(function () {
+
+  // ============== БУРГЕР =======================================
+
+  document.addEventListener('click', burgerInit)
+
+  function burgerInit(e) {
+
+    const burgerIcon = e.target.closest('.burger-icon')
+    const burgerNavLink = e.target.closest('.nav__link')
+
+    if (!burgerIcon && !burgerNavLink) return
+    if (document.documentElement.clientWidth > 900) return
+
+    // if (burgerIcon) {
+    //   e.preventDefault()
+    // }
+
+    if (!document.body.classList.contains('body--opened-menu')) {
+      document.body.classList.add('body--opened-menu')
+    } else document.body.classList.remove('body--opened-menu')
+
+
+  }
+
+  // ==========MODAL моё====================================================
+
+  const btn = document.querySelector('.about__img-btn')
+  const modal = document.querySelector('.modal')
+  const body = document.body
+  const cancel = document.querySelector('.modal__cancel')
+
+  btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    body.classList.add('body--opened-modal')
+  })
+
+  const closeModal = () => {
+    body.classList.remove('body--opened-modal')
+  }
+
+  document.addEventListener('click', (e) => {
+    e.preventDefault()
+    const target = e.target
+    if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+      closeModal()
+    }
+  })
+
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+      closeModal()
+    }
+  })
+
+
+  // ========== ТАБЫ ====================================================
+
+  const tabControls = document.querySelector('.tab-controls')
+
+  tabControls.addEventListener('click', toggleTab)
+
+  function toggleTab(e) {
+
+    const tabControl = e.target.closest('.tab-controls__link')
+
+    if (!tabControl) return
+    e.preventDefault()
+    if (tabControl.classList.contains('tab-controls__link--active')) return
+
+    const tabContentID = tabControl.getAttribute('href')
+    const tabContent = document.querySelector(tabContentID)
+    const activeControl = document.querySelector('.tab-controls__link--active')
+    const activeContent = document.querySelector('.tab-content--show')
+
+    if (activeControl) {
+      activeControl.classList.remove('tab-controls__link--active')
+    }
+    if (activeContent) {
+      activeContent.classList.remove('tab-content--show')
+    }
+
+    tabControl.classList.add('tab-controls__link--active')
+    tabContent.classList.add('tab-content--show')
+  }
+
+  // ========== АККОРДЕОН ====================================================
+
+  const accordionLists = document.querySelectorAll('.accordion-list');
+
+  accordionLists.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+
+      const accordionList = e.currentTarget
+      const accordeonOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+      const accordeonOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+      const accordeonControl = e.target.closest('.accordion-list__control');
+      if (!accordeonControl) return
+      const accordeonItem = accordeonControl.parentElement;
+      const accordeonContent = accordeonControl.nextElementSibling;
+
+      if (accordeonOpenedItem && accordeonItem != accordeonOpenedItem) {
+        accordeonOpenedItem.classList.remove('accordion-list__item--opened');
+        accordeonOpenedContent.style.maxHeight = null;
+      }
+
+      accordeonItem.classList.toggle('accordion-list__item--opened');
+
+      if (accordeonItem.classList.contains('accordion-list__item--opened')) {
+        accordeonContent.style.maxHeight = accordeonContent.scrollHeight + 'px';
+      } else {
+        accordeonContent.style.maxHeight = null;
+      }
+    })
+  })
+
+  // ========== СЛАЙДЕР-галерея ====================================================
+
+  const swiper = new Swiper('.gallery__slider', {
+
+    spaceBetween: 15,
+    slidesPerView: 1.5,
+
+    pagination: {
+      el: '.gallery__pagination',
+      type: 'fraction',
+    },
+
+    navigation: {
+      nextEl: '.gallery__next',
+      prevEl: '.gallery__prev',
+    },
+
+    breakpoints: {
+      601: {
+        slidesPerView: 3,
+      },
+      801: {
+        spaceBetween: 32,
+      },
+      1101: {
+        slidesPerView: 4,
+      }
+    }
+
+
+  });
+})()
+
+
+
+// ==========MODAL от куратора====================================================
+
+
+// const btn = document.querySelector('.about__img-btn')
+// const modal = document.querySelector('.modal')
+
+// btn.addEventListener('click', openModal)
+// modal.addEventListener('click', closeModal)
+
+// function openModal(e) {
+//   e.preventDefault()
+//   document.body.classList.toggle('body--opened-modal')
+// }
+
+// function closeModal(e) {
+//   e.preventDefault()
+//   const target = e.target
+//   if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+//     document.body.classList.remove('body--opened-modal')
+//   }
+// }
+
